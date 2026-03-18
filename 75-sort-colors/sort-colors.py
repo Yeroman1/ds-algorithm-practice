498ba0ef91415bcb6f0190149434b7713aa57c8f
Yeroman1 <101729726+Yeroman1@users.noreply.github.com>
2026-03-18 14:33:35 +0300

    Time: 0 ms (100.00%) | Memory: 19.3 MB (52.89%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -3,4 +3,9 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        nums.sort()
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[j]<nums[i]:
+                    nums[j],nums[i]=nums[i],nums[j]
+        
+        return nums
