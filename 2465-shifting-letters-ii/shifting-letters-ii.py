c90beb4e807d3dc3257d23c220f620c4d27aeaf6
Yeroman1 <101729726+Yeroman1@users.noreply.github.com>
2026-04-04 12:12:32 +0300

    Time: 68 ms (25.15%) | Memory: 44 MB (6.13%) - LeetSync

--- 2465-shifting-letters-ii/shifting-letters-ii.py
+++ 2465-shifting-letters-ii/shifting-letters-ii.py
@@ -7,13 +7,11 @@ class Solution:
             if r+1<len(a):
                 a[r+1]-=1 if d==1 else -1
 
-        p=[0]*len(a)
-        p[0]=a[0]
-        for i in range(1, len(p)):
-            p[i]=p[i-1]+a[i]
+        for i in range(1, len(a)):
+            a[i]=a[i-1]+a[i]
 
         res=""
         for i in range(len(s)):
-            res+=chr((ord(s[i]) - ord('a') + p[i]) % 26 + ord('a'))
+            res+=chr((ord(s[i]) - ord('a') + a[i]) % 26 + ord('a'))
         
         return res
